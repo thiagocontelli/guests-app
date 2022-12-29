68cc99df069390a2472c8650348698c66ab691d5
Thiago <thiagocontellid@gmail.com>
2022-12-29 14:54:20 -0300

    refactor: rename the classes, xml files and ids

--- app/src/main/java/com/example/convidados/MainActivity.kt
+++ app/src/main/java/com/example/convidados/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.convidados
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
@@ -27,8 +28,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startActivity(Intent(applicationContext, GuestFormActivity::class.java))
         }
 
         setUpNavigation()
@@ -47,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_all_guests, R.id.nav_present, R.id.nav_absent
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
