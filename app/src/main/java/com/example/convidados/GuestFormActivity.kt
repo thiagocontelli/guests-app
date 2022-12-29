ccfbb3d0b437fd576a94dbb90f6df6bb5792e6ba
Thiago <thiagocontellid@gmail.com>
2022-12-29 15:29:41 -0300

    feat: ui created and binding added to GuestFormActivity

--- app/src/main/java/com/example/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/GuestFormActivity.kt
@@ -2,10 +2,24 @@ package com.example.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityGuestFormBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSubmit.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_submit) {
+            Toast.makeText(applicationContext, "Botão clicado", Toast.LENGTH_SHORT).show()
+        }
     }
 }
